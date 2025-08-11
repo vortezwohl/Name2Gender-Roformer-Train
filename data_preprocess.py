45d7bfd2d29274d18f896c71a1ec8bffa31f672e
吴子豪 <vortez.wohl@gmail.com>
2025-08-11 18:11:28 +0800

    feat: 数据预处理和训练脚本构建

---
+++ data_preprocess.py
@@ -0,0 +1,32 @@
+import torch
+from datasets import load_dataset
+
+from model import tokenizer
+
+ds = load_dataset("erickrribeiro/gender-by-name")
+train_dataset, test_dataset = ds['train'], ds['test']
+
+
+def encode(text: str, batched: bool = False, device: torch.device = None) -> torch.Tensor:
+    if batched:
+        return torch.tensor([tokenizer.encode(text)], dtype=torch.long, device=device)
+    return torch.tensor(tokenizer.encode(text), dtype=torch.long, device=device)
+
+
+def preprocess(dataset: dict, dtype: torch.dtype = torch.float32, device: torch.device = torch.device('cpu'), **kwargs) -> tuple:
+    names, genders = dataset['Name'], dataset['Gender']
+    male_names, female_names = [], []
+    for i, name in enumerate(names):
+        if genders[i] == 0:
+            female_names.append(name)
+        else:
+            male_names.append(name)
+    all_texts = male_names + female_names
+    token_ids = [encode(x, **kwargs) for x in all_texts]
+    labels = ([torch.tensor([1.], dtype=dtype, device=device) for _ in range(len(male_names))]
+              + [torch.tensor([.0], dtype=dtype, device=device) for _ in range(len(female_names))])
+    return token_ids, labels
+
+
+if __name__ == '__main__':
+    print(preprocess(test_dataset.to_dict()))
